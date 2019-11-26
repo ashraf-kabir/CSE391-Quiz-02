0f7290169b33d103fc2ce511b7c8b7ce2fd14b19
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-11-26 19:03:20 +0600

    Update search.php

--- search.php
+++ search.php
@@ -26,37 +26,37 @@ include('config.php');
         </thead>
         <tbody>
             <?php
-    if (isset($_POST['submit1'])) {
-        $name = $_POST['search'];
-        $sql = "SELECT * FROM employee WHERE `name` LIKE :name";
-        $name = "%$name%";
-        $query = $dbh->prepare($sql);
-        $query->bindValue(':name', $name);
-        $query->execute();
-        $results = $query->fetchAll(PDO::FETCH_ASSOC);
-        if ($query->rowCount() > 0) {
-            foreach ($results as $row) {
-                $id = $row['id'];
-                $username = $row['name'];
-                $dept = $row['dept'];
-                $salary = $row['salary'];
-                $joindate = $row['creationdate'];
-                $rank = $row['rank'];
-                $leavecount = $row['leavec']; ?>
-            <tr>
-                <td><?php echo $id; ?></td>
-                <td><?php echo $username; ?></td>
-                <td><?php echo $dept; ?></td>
-                <td><?php echo $salary; ?></td>
-                <td><?php echo $joindate; ?></td>
-                <td><?php echo $rank; ?></td>
-                <td><?php echo $leavecount; ?></td>
-            </tr>
-            <?php
+            if (isset($_POST['submit1'])) {
+                $name = $_POST['search'];
+                $sql = "SELECT * FROM employee WHERE `name` LIKE :name";
+                $name = "%$name%";
+                $query = $dbh->prepare($sql);
+                $query->bindValue(':name', $name);
+                $query->execute();
+                $results = $query->fetchAll(PDO::FETCH_ASSOC);
+                if ($query->rowCount() > 0) {
+                    foreach ($results as $row) {
+                        $id = $row['id'];
+                        $username = $row['name'];
+                        $dept = $row['dept'];
+                        $salary = $row['salary'];
+                        $joindate = $row['creationdate'];
+                        $rank = $row['rank'];
+                        $leavecount = $row['leavec']; ?>
+                        <tr>
+                            <td><?php echo $id; ?></td>
+                            <td><?php echo $username; ?></td>
+                            <td><?php echo $dept; ?></td>
+                            <td><?php echo $salary; ?></td>
+                            <td><?php echo $joindate; ?></td>
+                            <td><?php echo $rank; ?></td>
+                            <td><?php echo $leavecount; ?></td>
+                        </tr>
+                        <?php
+                    }
+                }
             }
-        }
-    }
-    ?>
+            ?>
         </tbody>
     </table>
     <br>
